e44e279c3040b527facd4c7616f2c5523592d20b
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-01 06:00:30 -0700

    add hero section to root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
-
-
-export default function Home() {
+export default async function Home() {
   return (
     <>
-      HOME
+      <section className="hero_container">
+        <p className="tag">PITCH, VOTE, AND GROW</p>
+        <h1 className="heading">
+          SHARE AND DISCOVER <br />INNOVATIVE WEB APP IDEAS
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit your concepts, gather feedback, and collaborate with a vibrant community.
+        </p>
+      </section>
     </>
   );
 }
