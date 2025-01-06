232d88248fbc83700f21b345c77faf826ed781b2
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-06 13:58:37 -0600

    refactor Home component to destructure  and pass query to SearchForm

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,9 @@
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams } : {searchParams : Promise <{query?: string}>}) {
+
+  const query = (await searchParams).query
+
   return (
     <>
       <section className="hero_container">
@@ -11,7 +14,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit your concepts, gather feedback, and collaborate with a vibrant community.
         </p>
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
     </>
   );
