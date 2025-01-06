7a9ef49d6b3ccdbe4cc48ea01eceb80d9addde30
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-06 14:09:24 -0600

    update import paths

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/NavBar";
+import Navbar from "../../components/NavBar";
 
 export default function Layout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
