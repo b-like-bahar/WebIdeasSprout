f6f3d4a6fd7bfcd88f0d9797f048d4db4d0b2938
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-06 19:00:48 -0600

    add Webapps section and defined a hypothetical posts array in the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import SearchForm from "../../components/SearchForm";
+import WebappCard from "@/components/WebappCard";
 
 export default async function Home({ searchParams } : {searchParams : Promise <{query?: string}>}) {
 
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt: "Today",
+    views: 55,
+    author: { _id: 1 , name:"Bahar"},
+    _id: 1,
+    description: "This is a description",
+    image: "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fA%3D%3D",
+    category: "Educational",
+    title: "We Robots",
+  }
+];
 
   return (
     <>
@@ -21,6 +34,15 @@ export default async function Home({ searchParams } : {searchParams : Promise <{
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Websites"}
         </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+          posts.map((post: WebappCardType) => (
+            <WebappCard key={post?._id} post={post}/>
+          )) 
+        ) : (
+            <p className="no-result">No startups found</p>
+        )}
+        </ul>
       </section>
     </>
   );
