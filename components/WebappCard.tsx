41e618c2f25b811dfcf3070384b5554133bde984
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-06 19:25:54 -0600

    add views and date section to webapp card

--- components/WebappCard.tsx
+++ components/WebappCard.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { formatDate } from "@/lib/utils";
+import { EyeIcon } from "lucide-react"
 
-const WebappCard = () => {
+const WebappCard = ({post} : {post : WebappCardType}) => {
+    const { _createdAt, views, author: {authorId, name}, title, category, _id, image, description} = post;
+
     return (
-        <div>WebappCard</div>
+        <li className="webapp-card group">
+            <div className="flex-between">
+                <p className="webapp_card_date">
+                    {formatDate(_createdAt)}
+                </p>
+                <div className="flex gap-1.5">
+                    <EyeIcon className="size-6 text-primary" />
+                    <span className="text-16-medium">{views}</span>
+                </div>
+            </div>
+        </li>
     )
 }
 
