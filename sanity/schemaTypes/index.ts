587d0d478d539718949498fb1f1f5ed54012d182
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-10 18:51:28 -0600

    add author and webapp schemas to index.ts and structure.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { author } from '@/sanity/schemaTypes/author'
+import { webapp } from '@/sanity/schemaTypes/webapp'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, webapp],
 }
