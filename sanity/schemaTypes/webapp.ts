d55fc889c9f68a3b2ccdfdb460bd1a197706b3ff
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-10 18:46:38 -0600

    create webapp schema in sanity/schemaTypes

---
+++ sanity/schemaTypes/webapp.ts
@@ -0,0 +1,43 @@
+import {defineType, defineField} from "sanity"
+
+export const webapp = defineType({
+    name: "webapp",
+    title: "Webapp",
+    type: "document",
+    fields: [
+        defineField({
+            name: "slug",
+            type: "slug",
+            options: {
+                source: "title"
+            }
+        }),
+        defineField({
+            name: "author",
+            type: "reference",
+            to: { type: "author" },
+        }),
+        defineField({
+            name: "views",
+            type: "number",
+        }),
+        defineField({
+            name: "description",
+            type: "text",
+        }),
+        defineField({
+            name: "category",
+            type: "string",
+            validation: (Rule) => Rule.min(1).max(20).required.error("Please enter a category")
+        }),
+        defineField({
+            name: "image",
+            type: "url",
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: "pitch",
+            type: "markdown",
+        }),
+    ],
+});
