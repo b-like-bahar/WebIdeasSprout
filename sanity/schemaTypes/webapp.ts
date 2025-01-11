dfcf3262f97f268d1bd52766e5567825fe1396f5
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-10 19:29:03 -0600

    add title filed to webapp schema

--- sanity/schemaTypes/webapp.ts
+++ sanity/schemaTypes/webapp.ts
@@ -6,6 +6,10 @@ export const webapp = defineType({
     type: "document",
     fields: [
         defineField({
+            name: "title",
+            type: "string",
+        }),
+        defineField({
             name: "slug",
             type: "slug",
             options: {
