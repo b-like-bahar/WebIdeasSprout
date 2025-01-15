6800cb7a6cce5358fb790f542803e19a6c78eea5
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-15 16:24:06 -0600

    create WebappTypeCard based on generated types

--- components/WebappCard.tsx
+++ components/WebappCard.tsx
@@ -2,10 +2,22 @@ import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { EyeIcon } from "lucide-react"
+import { EyeIcon } from "lucide-react";
+import { Author, Webapp } from "@/sanity/types";
 
-const WebappCard = ({post} : {post : WebappCardType}) => {
-    const { _createdAt, views, author: {authorId, name}, title, category, _id, image, description} = post;
+export type WebappTypeCard = Omit<Webapp, "author"> & { author?: Author };
+
+const WebappCard = ({post} : { post : WebappTypeCard }) => {
+    const { 
+        _createdAt, 
+        views, 
+        author,
+        title, 
+        category, 
+        _id, 
+        image, 
+        description,
+    } = post;
 
     return (
         <li className="webapp-card group">
@@ -20,14 +32,14 @@ const WebappCard = ({post} : {post : WebappCardType}) => {
             </div>
             <div className="flex-between mt-5 gap-5">
                 <div className="flex-1">
-                    <Link href={`/user/${authorId}`}>
-                        <p className="text-16-medium line-clamp-1">{name}</p>
+                    <Link href={`/user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1">{author?.name}</p>
                     </Link>
                     <Link href={`/webapp/${_id}`}>
                         <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
                 </div>
-                <Link href={`/user/${authorId}`}>
+                <Link href={`/user/${author?._id}`}>
                     <Image src="https://placehold.co/48x48" alt="placeholder" width={48} height={48} className="rounded-full" />
                 </Link>
             </div>
@@ -38,7 +50,7 @@ const WebappCard = ({post} : {post : WebappCardType}) => {
                 <img src={image} alt={`${title} website image`} className="webapp-card_img" />
             </Link>
             <div className="flex-between gap-3 mt-5">
-                <Link href={`/?query=${category.toLowerCase()}`} >
+                <Link href={`/?query=${category?.toLowerCase()}`} >
                     <p className="text-16-medium">{category}</p>
                 </Link>
                 <Button className="webapp-card-btn" asChild>
