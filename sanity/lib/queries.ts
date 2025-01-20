8517e5ecf36af7f39cd56f0397017761d6eda0b0
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-20 15:09:34 -0600

    enhance WEBAPPS_QUERY to support dynamic search filters containing author name, category, and title

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const WEBAPPS_QUERY =
-    defineQuery(`*[_type == "webapp" && defined(slug.current)] | order(_createdAt desc){
+    defineQuery(`*[_type == "webapp" && defined(slug.current) && !defined($search) || title match $search || category match $search || author -> name match $search] | order(_createdAt desc){
         _id,
         title,
         slug,
