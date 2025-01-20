a596637aab79c91ed9e263c6b0f54d7926c5d07d
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-20 17:18:07 -0600

    initialize the webapps detail page with their unique id

---
+++ app/(root)/webapp/[id]/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Page = async ({ params }: { params: Promise <{ id: string }>}) => {
+    const id = (await params).id;
+
+  return (
+    <>
+    <h1 className="text-3xl">This is a webapp number: {id}</h1>
+    </>
+  )
+}
+
+export default Page
