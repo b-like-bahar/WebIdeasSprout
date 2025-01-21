031d6b068fd7505d3a7a02a7079dff58a1136d3e
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-21 16:52:07 -0600

    fetch views for webapp detail page in View component

--- components/View.tsx
+++ components/View.tsx
@@ -1,19 +1,23 @@
+import { client } from '@/sanity/lib/client'
+import { WEBAPP_VIEWS_QUERY } from '@/sanity/lib/queries'
 import React from 'react'
 
-const View = ({ id }: { id: string }) => {
+const View = async ({ id }: { id: string }) => {
+    const { views } = await client.withConfig({ useCdn: false }).fetch(WEBAPP_VIEWS_QUERY, { id })
+
     return (
         <div className="view-container">
             <div className="absolute -top-2 -right-2">
                 <div className="relative">
                     <div className="absolute -left-4 top-1">
                         <span className="flex size-[11px]">
-                            <span className= "absolute inline-flex h-full w-full animate-ping rounded-full bg-primary"></span>
+                            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-primary"></span>
                         </span>
                     </div>
                 </div>
             </div>
             <p className="view-text">
-                <span className="font-black">100views</span>
+                <span className="font-black">Views: {views}</span>
             </p>
         </div>
     )
