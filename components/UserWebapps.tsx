3cd492d7ed4e226fb77161103e3aa056a6c28960
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-24 16:02:22 -0600

    add UserWebapps component to fetch and display web apps by author id with fallback for no posts

---
+++ components/UserWebapps.tsx
@@ -0,0 +1,24 @@
+import { client } from '@/sanity/lib/client'
+import { WEBAPPS_BY_AUTHOR_QUERY } from '@/sanity/lib/queries'
+import React from 'react'
+import WebappCard, { WebappTypeCard } from './WebappCard'
+import { cn } from '@/lib/utils';
+import { Skeleton } from './ui/skeleton';
+
+const UserWebapps = async({ id }: {id : string}) => {
+
+    const webapps = await client.fetch(WEBAPPS_BY_AUTHOR_QUERY, { id })
+    return (
+        <>
+        {webapps.length > 0 ? (
+            webapps.map((webapp: WebappTypeCard) => (
+            <WebappCard key={webapp._id} post={webapp} />
+        ))
+    ) : (
+            <p className="no-result">No posts yet</p>
+        )}
+        </>
+    );
+};
+
+export default UserWebapps
