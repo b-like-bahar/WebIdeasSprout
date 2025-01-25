e3bb7f085ef8de6016322ba4a3f7eb43080f55f2
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-25 16:25:29 -0600

    add playlist schema to sanity schema definitions

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { author } from '@/sanity/schemaTypes/author'
 import { webapp } from '@/sanity/schemaTypes/webapp'
+import { playlist } from '@/sanity/schemaTypes/playlist'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author, webapp],
+  types: [author, webapp, playlist],
 }
