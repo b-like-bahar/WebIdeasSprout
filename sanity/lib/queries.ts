082f1ed46264e1b2078e39bbd15002365973cac5
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-25 16:19:26 -0600

    create PLAYLIST_BY_SLUG_QUERY query for fetching playlist details by slug with associatedselect fields

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -73,3 +73,27 @@ export const WEBAPPS_BY_AUTHOR_QUERY =
         category,
         image
 }`);
+
+export const PLAYLIST_BY_SLUG_QUERY = defineQuery(`
+*[_type == "playlist" && slug.current == $slug][0]{
+    _id,
+    title,
+    slug,
+    select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+    _id,
+    name,
+    slug,
+    image,
+    bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch}
+}`);
