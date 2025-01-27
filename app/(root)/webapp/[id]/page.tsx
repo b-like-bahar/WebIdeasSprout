f5059b4c73387196c5328dff97e40612192600e2
Bahar Hamzeh <blikebahar76@gmail.com>
2025-01-27 17:44:30 -0600

    add optional chaining for author properties in webapp details page

--- app/(root)/webapp/[id]/page.tsx
+++ app/(root)/webapp/[id]/page.tsx
@@ -39,10 +39,10 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 <div className="space-y-5  mt-10 max-w-4xl mx-auto">
                     <div className="flex-between gap-5">
                         <Link href={`/user/${post.author?._id}`} className="flex gap-2 items-center mb-3">
-                            <Image src={post.author.image || "https://placehold.co/64x64"} alt="avatar" width={64} height={64} className="rounded-full drop-shadow-lg" />
+                            <Image src={post.author?.image || "https://placehold.co/64x64"} alt="avatar" width={64} height={64} className="rounded-full drop-shadow-lg" />
                             <div>
-                                <p className="text-20-medium">{post.author.name}</p>
-                                <p className="text-16-medium !text-black-300">@{post.author.username}</p>
+                                <p className="text-20-medium">{post.author?.name}</p>
+                                <p className="text-16-medium !text-black-300">@{post.author?.username}</p>
                             </div>
                         </Link>
                         <p className="category-tag">{post.category}</p>
